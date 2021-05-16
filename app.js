bfbf5bf67d75e29531a39510930404bca2f1b69b
tinahung126 <tina126hung@gmail.com>
2021-05-16 13:59:36 +0800

    feat:fix heroku error

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const PORT = process.env.PORT || 3000
-const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const routes = require('./routes')
+const PORT = process.env.PORT || 3000;
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const routes = require('./routes');
 const helper = exphbs.create({
   defaultlayout: 'main',
   extname: 'hbs',
   helpers: {
     eq: function (v1, v2) {
-      return v1 === v2
-    }
-  }
-})
-require('./config/mongoose')
-app.engine('hbs', helper.engine)
-app.set('view engine', 'hbs')
+      return v1 === v2;
+    },
+  },
+});
+require('./config/mongoose');
+app.engine('hbs', helper.engine);
+app.set('view engine', 'hbs');
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 // setting static files
-app.use(express.static('public'))
-app.use(methodOverride('_method'))
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
-app.use(routes)
+app.use(routes);
 app.listen(PORT, () => {
-  console.log(`The express server is running on http://localhost:${PORT}`)
-})
+  console.log(`The express server is running on http://localhost:${PORT}`);
+});
