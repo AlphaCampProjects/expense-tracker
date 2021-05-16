b467d5af391b8f8c286a41b3d2b9ca12d134ac3c
tinahung126 <tina126hung@gmail.com>
2021-05-16 15:16:13 +0800

    feat:fix heroku error

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,35 +1,35 @@
-const Category = require('../category')
-const db = require('../../config/mongoose')
+const db = require('../../config/mongoose');
+const Category = require('../category');
 
 const data = [
   {
     name: '飲食',
-    icon: 'fas fa-utensils'
+    icon: 'fas fa-utensils',
   },
   {
     name: '休閒娛樂',
-    icon: 'fas fa-grin-beam'
+    icon: 'fas fa-grin-beam',
   },
   {
     name: '交通',
-    icon: 'fas fa-shuttle-van'
+    icon: 'fas fa-shuttle-van',
   },
   {
     name: '家庭',
-    icon: 'fas fa-home'
+    icon: 'fas fa-home',
   },
   {
     name: '其它',
-    icon: 'fas fa-pen'
-  }
-]
+    icon: 'fas fa-pen',
+  },
+];
 
 db.once('open', () => {
   for (let i = 0; i < data.length; i++) {
     Category.create({
       name: data[i].name,
-      icon: data[i].icon
-    })
+      icon: data[i].icon,
+    });
   }
-  console.log('Category Done')
-})
+  console.log('Category Done');
+});
